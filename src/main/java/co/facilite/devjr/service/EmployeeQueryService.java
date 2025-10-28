a2103150c2531cc7f13a004fd9307b80089a2162
pedro-rods <104791600+pedro-rods@users.noreply.github.com>
2025-10-28 14:01:05 -0300

    Retornando este arquivo para o seu estado original
    por algum motivo antes as entidade com _ (ex: employee_) não estavam sendo reconhecidas, mas agora o código decidiu que iria funcionar(?) então estou retornando este arquivo para seu estado original

--- src/main/java/co/facilite/devjr/service/EmployeeQueryService.java
+++ src/main/java/co/facilite/devjr/service/EmployeeQueryService.java
@@ -1,97 +1,90 @@
 package co.facilite.devjr.service;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.jpa.domain.Specification;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-
-// for static metamodels
+import co.facilite.devjr.domain.*; // for static metamodels
 import co.facilite.devjr.domain.Employee;
 import co.facilite.devjr.repository.EmployeeRepository;
 import co.facilite.devjr.service.criteria.EmployeeCriteria;
 import co.facilite.devjr.service.dto.EmployeeDTO;
 import co.facilite.devjr.service.mapper.EmployeeMapper;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import jakarta.persistence.criteria.JoinType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tech.jhipster.service.QueryService;
 
 /**
- * Service for executing complex queries for {@link Employee} entities in the
- * database. The main input is a {@link EmployeeCriteria} which gets converted
- * to {@link Specification}, in a way that all the filters must apply. It
- * returns a {@link Page} of {@link EmployeeDTO} which fulfills the criteria.
+ * Service for executing complex queries for {@link Employee} entities in the database.
+ * The main input is a {@link EmployeeCriteria} which gets converted to {@link Specification},
+ * in a way that all the filters must apply.
+ * It returns a {@link Page} of {@link EmployeeDTO} which fulfills the criteria.
  */
-@Slf4j
 @Service
 @Transactional(readOnly = true)
-@RequiredArgsConstructor
 public class EmployeeQueryService extends QueryService<Employee> {
 
-	private final EmployeeRepository employeeRepository;
+    private static final Logger LOG = LoggerFactory.getLogger(EmployeeQueryService.class);
 
-//	private final EmployeeMapper employeeMapper;
+    private final EmployeeRepository employeeRepository;
 
-	/**
-	 * Return a {@link Page} of {@link EmployeeDTO} which matches the criteria from
-	 * the database.
-	 * 
-	 * @param criteria The object which holds all the filters, which the entities
-	 *                 should match.
-	 * @param page     The page, which should be returned.
-	 * @return the matching entities.
-	 */
-	@Transactional(readOnly = true)
-	public Page<EmployeeDTO> findByCriteria(EmployeeCriteria criteria, Pageable page) {
-		log.debug("find by criteria : {}, page: {}", criteria, page);
-		final Specification<Employee> specification = createSpecification(criteria);
-		return null;
-//		return employeeRepository.findAll(specification, page).map(employeeMapper::toDto);
-	}
+    private final EmployeeMapper employeeMapper;
 
-	/**
-	 * Return the number of matching entities in the database.
-	 * 
-	 * @param criteria The object which holds all the filters, which the entities
-	 *                 should match.
-	 * @return the number of matching entities.
-	 */
-	@Transactional(readOnly = true)
-	public long countByCriteria(EmployeeCriteria criteria) {
-		log.debug("count by criteria : {}", criteria);
-		final Specification<Employee> specification = createSpecification(criteria);
-		return employeeRepository.count(specification);
-	}
+    public EmployeeQueryService(EmployeeRepository employeeRepository, EmployeeMapper employeeMapper) {
+        this.employeeRepository = employeeRepository;
+        this.employeeMapper = employeeMapper;
+    }
 
-	/**
-	 * Function to convert {@link EmployeeCriteria} to a {@link Specification}
-	 * 
-	 * @param criteria The object which holds all the filters, which the entities
-	 *                 should match.
-	 * @return the matching {@link Specification} of the entity.
-	 */
-	protected Specification<Employee> createSpecification(EmployeeCriteria criteria) {
-//		Specification<Employee> specification = Specification.where(null);
-//		if (criteria != null) {
-//			specification = Specification.allOf(
-//					Boolean.TRUE.equals(criteria.getDistinct()) ? distinct(criteria.getDistinct()) : null,
-//
-//					// campos simples
-//					buildRangeSpecification(criteria.getId(), "id"),
-//					buildStringSpecification(criteria.getFirstName(), "firstName"),
-//					buildStringSpecification(criteria.getLastName(), "lastName"),
-//					buildStringSpecification(criteria.getEmail(), "email"),
-//					buildStringSpecification(criteria.getPhone(), "phone"),
-//					buildRangeSpecification(criteria.getHireDate(), "hireDate"),
-//					buildRangeSpecification(criteria.getSalary(), "salary"),
-//					buildSpecification(criteria.getActive(), "active"),
-//
-//					// relacionamentos (joins por nome)
-//					buildSpecification(criteria.getAddressId(), root -> root.join("address", JoinType.LEFT).get("id")),
-//					buildSpecification(criteria.getDepartmentId(),
-//							root -> root.join("department", JoinType.LEFT).get("id")));
-//		}
-		return null;
-//		return specification;
-	}
+    /**
+     * Return a {@link Page} of {@link EmployeeDTO} which matches the criteria from the database.
+     * @param criteria The object which holds all the filters, which the entities should match.
+     * @param page The page, which should be returned.
+     * @return the matching entities.
+     */
+    @Transactional(readOnly = true)
+    public Page<EmployeeDTO> findByCriteria(EmployeeCriteria criteria, Pageable page) {
+        LOG.debug("find by criteria : {}, page: {}", criteria, page);
+        final Specification<Employee> specification = createSpecification(criteria);
+        return employeeRepository.findAll(specification, page).map(employeeMapper::toDto);
+    }
+
+    /**
+     * Return the number of matching entities in the database.
+     * @param criteria The object which holds all the filters, which the entities should match.
+     * @return the number of matching entities.
+     */
+    @Transactional(readOnly = true)
+    public long countByCriteria(EmployeeCriteria criteria) {
+        LOG.debug("count by criteria : {}", criteria);
+        final Specification<Employee> specification = createSpecification(criteria);
+        return employeeRepository.count(specification);
+    }
+
+    /**
+     * Function to convert {@link EmployeeCriteria} to a {@link Specification}
+     * @param criteria The object which holds all the filters, which the entities should match.
+     * @return the matching {@link Specification} of the entity.
+     */
+    protected Specification<Employee> createSpecification(EmployeeCriteria criteria) {
+        Specification<Employee> specification = Specification.where(null);
+        if (criteria != null) {
+            // This has to be called first, because the distinct method returns null
+            specification = Specification.allOf(
+                Boolean.TRUE.equals(criteria.getDistinct()) ? distinct(criteria.getDistinct()) : null,
+                buildRangeSpecification(criteria.getId(), Employee_.id),
+                buildStringSpecification(criteria.getFirstName(), Employee_.firstName),
+                buildStringSpecification(criteria.getLastName(), Employee_.lastName),
+                buildStringSpecification(criteria.getEmail(), Employee_.email),
+                buildStringSpecification(criteria.getPhone(), Employee_.phone),
+                buildRangeSpecification(criteria.getHireDate(), Employee_.hireDate),
+                buildRangeSpecification(criteria.getSalary(), Employee_.salary),
+                buildSpecification(criteria.getActive(), Employee_.active),
+                buildSpecification(criteria.getAddressId(), root -> root.join(Employee_.address, JoinType.LEFT).get(Address_.id)),
+                buildSpecification(criteria.getDepartmentId(), root -> root.join(Employee_.department, JoinType.LEFT).get(Department_.id))
+            );
+        }
+        return specification;
+    }
 }
